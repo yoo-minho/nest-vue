5a7afdba2183ec8b393835a51937ca34adbd7a06
유민호 <wingedcompany@gmail.com>
2022-07-25 01:06:23 +0900

    얼추 validator 달았다.

--- src/group/dto/create-group.dto.ts
+++ src/group/dto/create-group.dto.ts
@@ -1,12 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateGroupDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   domain: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   title: string;
 
+  @IsString()
   @ApiProperty({ required: false })
   description?: string;
 
