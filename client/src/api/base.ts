950e98c49fe3bb2e97cdb67faa1f581bba919b4d
유민호 <wingedcompany@gmail.com>
2022-08-09 01:58:52 +0900

    통계 로딩만...

--- client/src/api/base.ts
+++ client/src/api/base.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useAxios } from '@vueuse/integrations/useAxios';
 
 const axiosClient = axios.create({
   baseURL: 'http://localhost:5000/api',
@@ -29,3 +30,6 @@ axiosClient.interceptors.response.use(
 );
 
 export default axiosClient;
+export const useAxiosGet = (url: string) => useAxios(url, { method: 'GET' }, axiosClient);
+export const useAxiosPost = (url: string, data: any) => useAxios(url, { method: 'POST', data }, axiosClient);
+export const useAxiosDelete = (url: string) => useAxios(url, { method: 'DELETE' }, axiosClient);
