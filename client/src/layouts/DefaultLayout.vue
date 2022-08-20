4f9504c59783781316c0e43f2d7056773c7450ff
유민호 <wingedcompany@gmail.com>
2022-08-21 00:30:22 +0900

    레이아웃

---
+++ client/src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HeaderItem from '@/components/Menu/HeaderItem.vue';
+</script>
+
+<template>
+  <q-layout>
+    <HeaderItem :logo="true" :editor="true" :setting="true" />
+    <q-page-container class="max-width">
+      <q-scroll-area :visible="false" class="max-width without-header">
+        <slot></slot>
+      </q-scroll-area>
+    </q-page-container>
+  </q-layout>
+</template>
--- client/src/views/GroupMain/GroupMain.vue
+++ client/src/views/GroupMain/GroupMain.vue
@@ -3,6 +3,8 @@ import { onMounted, watch } from 'vue';
 import { storeToRefs } from 'pinia';
 
 import { useGroupStore } from '@/stores/group';
+
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import HeaderItem from '@/components/Menu/HeaderItem.vue';
 import GroupCard from './components/GroupCard.vue';
 
@@ -29,38 +31,33 @@ watch(
 </script>
 
 <template>
-  <q-layout>
-    <HeaderItem :logo="true" :editor="true" :setting="true" />
-    <q-page-container class="max-width">
-      <q-scroll-area :visible="false" class="max-width without-header">
-        <q-scroll-area class="q-px-md q-pt-md tag-area" :thumb-style="{ opacity: '0' }">
-          <div class="row no-wrap">
-            <template v-if="tagsLoading">
-              <q-skeleton v-for="n in 3" :key="n" :type="'QChip'" class="q-ma-xs" />
-            </template>
-            <template v-else>
-              <q-chip
-                v-for="(tag, i) in NavTags"
-                :key="i"
-                :class="{ active: currentTag === tag.name }"
-                clickable
-                @click="setCurrentTag(tag.name)"
-                >{{ tag.name }}
-              </q-chip>
-            </template>
-          </div>
-        </q-scroll-area>
-        <q-page class="q-pa-md">
-          <template v-if="groupsLoading">
-            <q-skeleton v-for="n in 1" :key="n" :type="'QChip'" class="q-ma-xs" />
-          </template>
-          <template v-else>
-            <group-card v-for="group in groups" :key="group.id" :group="group" />
-          </template>
-        </q-page>
-      </q-scroll-area>
-    </q-page-container>
-  </q-layout>
+  <DefaultLayout>
+    <q-scroll-area class="q-px-md q-pt-md tag-area" :thumb-style="{ opacity: '0' }">
+      <div class="row no-wrap">
+        <template v-if="tagsLoading">
+          <q-skeleton v-for="n in 3" :key="n" :type="'QChip'" class="q-ma-xs" />
+        </template>
+        <template v-else>
+          <q-chip
+            v-for="(tag, i) in NavTags"
+            :key="i"
+            :class="{ active: currentTag === tag.name }"
+            clickable
+            @click="setCurrentTag(tag.name)"
+            >{{ tag.name }}
+          </q-chip>
+        </template>
+      </div>
+    </q-scroll-area>
+    <q-page class="q-pa-md">
+      <template v-if="groupsLoading">
+        <q-skeleton v-for="n in 1" :key="n" :type="'QChip'" class="q-ma-xs" />
+      </template>
+      <template v-else>
+        <group-card v-for="group in groups" :key="group.id" :group="group" />
+      </template>
+    </q-page>
+  </DefaultLayout>
 </template>
 
 <style scope>
