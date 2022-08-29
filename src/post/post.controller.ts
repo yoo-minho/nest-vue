851979a43e9058d59453bf0077efe3776e10f847
유민호 <wingedcompany@gmail.com>
2022-08-30 00:03:18 +0900

    캐싱매니저 + 조회겟방식

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -9,10 +9,16 @@ import {
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { Prisma } from '@prisma/client';
+import { CacheService } from 'src/cache/cache.service';
+
+const numbersPipe = new ParseArrayPipe({ items: Number, separator: ',' });
 
 @Controller('post')
 export class PostController {
-  constructor(private readonly postService: PostService) {}
+  constructor(
+    private readonly postService: PostService,
+    private readonly cacheService: CacheService,
+  ) {}
 
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
@@ -25,10 +31,7 @@ export class PostController {
   }
 
   @Get()
-  findAll(
-    @Query('linkIds', new ParseArrayPipe({ items: Number, separator: ',' }))
-    linkIds: number[],
-  ) {
+  findAll(@Query('linkIds', numbersPipe) linkIds: number[]) {
     return this.postService.posts({
       where: { linkId: { in: linkIds } },
       orderBy: { createdAt: 'desc' },
@@ -36,22 +39,21 @@ export class PostController {
     });
   }
 
-  @Post('last')
-  async findAllLast(@Body() { linkIds }) {
-    const linkArr = linkIds.map(({ linkId }) => linkId);
-    const lastPostArr = await this.postService.lastPosts(linkArr);
+  @Get('last')
+  async findAllLast(@Query('linkIds', numbersPipe) linkIds: number[]) {
+    const lastPostArr = await this.postService.lastPosts(linkIds);
+    const groupLinks = await this.cacheService.getGroupLinks();
     return lastPostArr.map(({ _max, linkId }) => {
       return {
         linkId,
         createdAt: _max.createdAt,
-        title: linkIds.find((v) => v.linkId === linkId).title,
+        title: groupLinks.find(({ link }) => link.id === linkId).link.title,
       };
     });
   }
 
-  @Post('count/date')
-  async countByDate(@Body() { linkIds }) {
-    const linkArr = linkIds.map(({ linkId }) => linkId);
-    return await this.postService.postCountByDate(linkArr);
+  @Get('count/date')
+  async countByDate(@Query('linkIds', numbersPipe) linkIds: number[]) {
+    return await this.postService.postCountByDate(linkIds);
   }
 }
