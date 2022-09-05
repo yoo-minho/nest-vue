effb4a0e68ef6e49248d6c8f2c045540e5b6ee3d
유민호 <wingedcompany@gmail.com>
2022-09-06 00:30:32 +0900

    저장해보자으 아직 varchar 해야함

--- client/src/util/ImageUtil.ts
+++ client/src/util/ImageUtil.ts
@@ -1,19 +1,18 @@
 import platformArray from '@/data/platform.json';
+import { BlogType } from '@/types/common';
 
-export const getBlogType = (url = ''): string =>
-  platformArray
-    .map((platform) => platform.name.toUpperCase())
-    .find((platform) => url.toUpperCase().includes(platform)) || '';
+export const getBlogType = (url = ''): BlogType =>
+  (platformArray.map((p) => p.type).find((platform) => url.toUpperCase().includes(platform)) || '') as BlogType;
 
-export const getBlogImagePath = (type: string) => {
-  const target = platformArray.filter((v) => v.name?.toUpperCase() === type?.toUpperCase());
+export const getBlogImagePath = (type: BlogType) => {
+  const target = platformArray.filter((p) => p.type === type?.toUpperCase());
   return target.length > 0 ? target[0].path : '';
 };
 
 export const getImage = (path: string) =>
   new URL(`../assets/images/${path || 'platform/rss.png'}`, import.meta.url).toString();
 
-export const getImageByBlogType = (type: string): string => getImage(getBlogImagePath(type));
+export const getImageByBlogType = (type: BlogType): string => getImage(getBlogImagePath(type));
 
 export const isTextImage = (url: string) => {
   if (url === 'https://t1.daumcdn.net/tistory_admin/static/images/openGraph/opengraph.png') {
