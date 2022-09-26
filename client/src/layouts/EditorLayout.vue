25bf92fe13c0c967b06a9fb554a7cc837e4b0c51
유민호 <wingedcompany@gmail.com>
2022-09-26 23:54:53 +0900

    스크롤 qa 처리

--- client/src/layouts/EditorLayout.vue
+++ client/src/layouts/EditorLayout.vue
@@ -9,11 +9,13 @@ const emits = defineEmits<{ (eventName: 'close'): void; (eventName: 'save'): voi
   <q-layout class="max-width subpage">
     <HeaderItem :close="() => emits('close')" :title="props.title" :save="() => emits('save')" />
     <q-page-container class="max-width">
-      <q-page class="q-pa-md">
-        <q-form class="q-gutter-y-md column">
-          <slot></slot>
-        </q-form>
-      </q-page>
+      <q-scroll-area :visible="false" class="max-width without-header">
+        <q-page class="q-pa-md" style="width: 100vw">
+          <q-form class="q-gutter-y-md column">
+            <slot></slot>
+          </q-form>
+        </q-page>
+      </q-scroll-area>
     </q-page-container>
   </q-layout>
 </template>
