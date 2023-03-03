734661d312537c2302246cee3db3ac1d6673d629
유민호 <wingedcompany@gmail.com>
2023-03-04 00:01:10 +0900

    여기까지는 기본처리

--- client/src/hooks/useKakaoApi.ts
+++ client/src/hooks/useKakaoApi.ts
@@ -4,7 +4,7 @@ export const shareKakao = () => {
     content: {
       title: '팀로그(teamlog) - 쓰던 블로그 그대로 팀 블로그 만들기',
       description: '티스토리, 미디엄, 브런치, 벨로그, 네이버블로그 상관없이 팀 블로그를 만들 수 있어요!',
-      imageUrl: 'https://teamlog.team/og_image.png',
+      imageUrl: 'https://teamlog.team/og_image_white.png',
       link: {
         mobileWebUrl: location.href,
         webUrl: location.href,
--- client/src/views/GroupMain/components/GroupAdBanner.vue
+++ client/src/views/GroupMain/components/GroupAdBanner.vue
@@ -5,7 +5,7 @@ const emits = defineEmits<{ (eventName: 'clickBanner'): void }>();
 
 <template>
   <q-banner class="bg-green-4 text-white q-mb-md cursor-pointer shadow-1" rounded dense @click="emits('clickBanner')">
-    <div style="display: flex; justify-content: space-between">
+    <div style="display: flex; justify-content: space-between; align-items: center">
       <q-icon class="col-1" :name="icon" size="24px" />
       <span class="col-8 q-mx-sm" style="font-size: 1em">{{ contents }}</span>
       <q-icon class="col-1" name="ads_click" size="24px" />
