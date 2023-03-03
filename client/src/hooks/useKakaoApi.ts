a9ab549f1f3bf0d19c4cf8642c5e438dc6550514
유민호 <wingedcompany@gmail.com>
2023-03-04 02:25:10 +0900

    seo 3

--- client/src/hooks/useKakaoApi.ts
+++ client/src/hooks/useKakaoApi.ts
@@ -2,9 +2,9 @@ export const shareKakao = () => {
   Kakao.Share.sendDefault({
     objectType: 'feed',
     content: {
-      title: '팀로그(teamlog) - 쓰던 블로그 그대로 팀 블로그 만들기',
+      title: '팀로그(teamlog) - 팀으로 글쓰기를 시작할때 최고의 선택',
       description: '티스토리, 미디엄, 브런치, 벨로그, 네이버블로그 상관없이 팀 블로그를 만들 수 있어요!',
-      imageUrl: 'https://teamlog.team/og_image_white.png',
+      imageUrl: 'https://teamlog.team/kakao_share.png',
       link: {
         mobileWebUrl: location.href,
         webUrl: location.href,
